bf45ef90f1c7fbae62126bd38d4aab9df9d877db
Dolores <koliada.mv@gmail.com>
2021-04-22 23:13:54 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.scss';
 import App from './pages/App';
 import Theory from './pages/Theory';
